0cef43a3b6360cc37a86f88b5520d35ae09dc0d1
sonledevplus <son.le@devplus.edu.vn>
2022-12-14 19:43:19 +0700

    completed introduce & contact page

--- shoesshop/src/pages/AboutUs/AboutUs.js
+++ shoesshop/src/pages/AboutUs/AboutUs.js
@@ -1,9 +1,54 @@
-import MainLayout from "../../layouts/MainLayout/MainLayout";
-import "./AboutUs.css";
+import { Link } from 'react-router-dom';
+import MainLayout from '../../layouts/MainLayout/MainLayout';
+import './AboutUs.css';
 function AboutUs() {
   return (
     <MainLayout>
-      <div className="aboutus"></div>
+      <div className='aboutus'>
+        <div className='container'>
+          <div className='aboutus__link'>
+            <Link to='/'>HOME </Link>
+            <span>/ </span>
+            <Link to='/aboutus' className='sub-link'>
+              CONNECT WITH US
+            </Link>
+          </div>
+          <div className='about__content'>
+            <h3 className='about__title'>CONNECT WITH US</h3>
+            <p className='about__des'>
+              Không dừng lại ở việc xây dựng nên một cửa hàng sneaker thông
+              thường. Rekeans còn là nơi chứa đựng rất nhiều những cảm hứng,
+              những ý tưởng và câu chuyện xoay quanh nền văn hoá sát mặt đất.
+              Hãy đến, trải nghiệm và cùng chia sẻ câu chuyện thú vị của bạn
+              xoay quanh những đôi giày nhé!
+            </p>
+            <div className='about__introduce d-flex'>
+              <h5 className='about__info'>Tại sao nên chọn Rekeans?</h5>
+              <div className='about__info-des'>
+                <p>Giao hàng toàn quốc. Giao nhanh khu vực Đà Nẵng.</p>Sản phẩm
+                luôn có sẵn tại cửa hàng. Không phải chờ order.
+                <p>Mẫu mới update thường xuyên mỗi tháng.</p>​Đảm bảo chất lượng
+                luôn đứng đầu trên thị trường.
+                <p>​Dễ dàng đổi trả nếu không vừa hoặc không hài lòng.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <img
+          src='https://sneakerholicvietnam.vn/wp-content/uploads/2022/10/dscf9895.jpg'
+          alt='banner-sneakers'
+          className='about__banner'
+        />
+        <div className='container'>
+          <div className='about__content'>
+            <h3 className='about__local'>LOCATION</h3>
+            <p className='about__address'>
+              69 Nguyen Van Linh, Hai Chau District, Da Nang City, Vietnam
+            </p>
+            <p className='about__phone'>Call +84969696969</p>
+          </div>
+        </div>
+      </div>
     </MainLayout>
   );
 }
--- shoesshop/src/pages/contact/Contact.js
+++ shoesshop/src/pages/contact/Contact.js
@@ -1,9 +1,39 @@
-import MainLayout from "../../layouts/MainLayout/MainLayout";
-import "./Contact.css";
+import MainLayout from '../../layouts/MainLayout/MainLayout';
+import './Contact.css';
 function Contact() {
   return (
     <MainLayout>
-      <div className="contact"></div>
+      <div className='contact'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-xxl-6 col-md-12 col-sm-12'>
+              <h3 className='contact__heading'>Trang liên hệ</h3>
+              <p className='contact__title'>Địa chỉ chúng tôi</p>
+              <p className='contact__content'>
+                69 Nguyen Van Linh, Hai Chau District, Da Nang City, Vietnam
+              </p>
+              <p className='contact__title'>Email chúng tôi</p>
+              <p className='contact__content'>rekeans@gmail.com</p>
+              <p className='contact__title'>Điện thoại</p>
+              <p className='contact__content'>+84969696969</p>
+              <p className='contact__title'>Thời gian làm việc</p>
+              <p className='contact__content'>10h00 - 22h00</p>
+            </div>
+            <div className='col-xxl-6 col-md-12 col-sm-12'>
+              <img
+                src='https://www.techrepublic.com/wp-content/uploads/2014/07/staticmapgoogle0514.png'
+                alt='maps'
+                className='banner__maps-img'
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <img
+        src='https://sneakerholicvietnam.vn/wp-content/uploads/2020/07/banner-sneakerholicvn2-1.jpg'
+        alt='banner'
+        className='contact__banner'
+      />
     </MainLayout>
   );
 }
